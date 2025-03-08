c903d563971ca75fce5c557660556adb8d0a6c99
Enes Demirci <20194604004@ogr.akdeniz.edu.tr>
2025-03-08 15:21:59 +0300

    8: feat: add CRUD functions for todos management in Pocketbase

--- lib/action.ts
+++ lib/action.ts
@@ -1,10 +1,33 @@
 "use server"
+// import { revalidatePath } from "next/cache";
 import { pb, Todo } from "./pocketbase";
 
 export const getTodos = async () => {
     const todos = await pb.collection("todos").getList(1, 30, {
         headers: { x_token: process.env.POCKETBASE_TOKEN! },
     });
-
     return todos?.items as Todo[];
 };
+
+export const createTodo = async (title: string) => {
+    const todo = await pb.collection("todos").create({
+        title,
+        completed: false,
+    });
+    // revalidatePath("/")
+    return todo as Todo;
+}
+
+export const deleteTodo = async (id: string) => {
+    await pb.collection("todos").delete(id);
+    // revalidatePath("/")
+    return { success: true }
+}
+
+export const updateTodo = async (id: string, completed: boolean) => {
+    await pb.collection("todos").update(id, {
+        completed,
+    });
+    // revalidatePath("/")
+    return { success: true }
+}
